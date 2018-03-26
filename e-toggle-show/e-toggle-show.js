e3a4ea1eb06870168591bc1a6e0d5bf65c0e1e42
Михаил О. Колосков <m.koloskov@live.ru>
2018-03-26 18:14:55 +0300

    Delete e-togle-show

--- e-toggle-show/e-toggle-show.js
+++
@@ -1,8 +0,0 @@
-$('.e-toggle-show__action').click(function() {
-  $('.e-toggle-show__more').slideUp('normal');
-  $('.e-toggle-show__action .text').text('Показать вопросы');
-  if($('.e-toggle-show__more').css('display') == 'none') {
-    $('.e-toggle-show__more').slideDown('normal');
-    $('.e-toggle-show__action .text').text('Скрыть вопросы');
-  }
-});
